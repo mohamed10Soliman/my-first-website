f4a5604a457b56f85dc719e7ccadba9c728586ad
mohamed <mahamedabdelmaksoud5280@gmail.com>
2026-04-22 18:25:41 +0200

    Number Guessing Game

---
+++ Number Guessing Game.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 42;
+            int attempts = 3;
+
+            for (int i = 1; i <= attempts; i++)
+            {
+                Console.Write("Enter your guess: ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine(" You win");
+                    return;
+                }
+                else if (guess > secretNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine("Too low");
+                }
+            }
+
+            Console.WriteLine("Game Over! The number was 42");
+        }
+    }
+}
+    
+
