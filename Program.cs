4657e951d7882cc8e0a219a6c4b29f7319a9fd9b
mohamed <mahamedabdelmaksoud5280@gmail.com>
2026-04-22 18:19:51 +0200

    FuzzBuzz

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          for (int i = 1 ; i <= 20 ;i++) 
+            { 
+              if (i % 3 == 0 && i % 5 == 0)
+                   Console.WriteLine($"{i} is FizzBuzz");
+              else if (i % 5 == 0) 
+                   Console.WriteLine($"{i} is Buzz");
+              else if (i % 3 == 0 )
+                    Console.WriteLine($"{i} is Fizz"); 
+              else 
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
